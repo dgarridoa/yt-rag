d5a191bc1e426e92a94a5a9ab7ad09d1e8fd36b6
Diego Alberto Garrido Amaya <diego.garrido.6568@gmail.com>
2024-09-01 14:29:16 -0400

    feat: add schemas module

---
+++ yt_rag/schemas.py
@@ -0,0 +1,57 @@
+from pyspark.sql.types import (
+    ArrayType,
+    DoubleType,
+    StringType,
+    StructField,
+    StructType,
+    TimestampType,
+)
+
+VideoTranscriptSchema = StructType(
+    [
+        StructField(
+            "video",
+            StructType(
+                [
+                    StructField("channel_id", StringType()),
+                    StructField("video_id", StringType()),
+                    StructField("title", StringType()),
+                    StructField("publish_time", TimestampType()),
+                ],
+            ),
+        ),
+        StructField(
+            "transcript",
+            ArrayType(
+                StructType(
+                    [
+                        StructField("start", DoubleType()),
+                        StructField("duration", DoubleType()),
+                        StructField("text", StringType()),
+                    ]
+                )
+            ),
+        ),
+    ]
+)
+TranscriptsSchema = StructType(
+    [
+        StructField("channel_id", StringType()),
+        StructField("video_id", StringType()),
+        StructField("title", StringType()),
+        StructField("publish_time", TimestampType()),
+        StructField("start", DoubleType()),
+        StructField("duration", DoubleType()),
+        StructField("text", StringType()),
+    ]
+)
+
+ContentSchema = StructType(
+    [
+        StructField("channel_id", StringType()),
+        StructField("video_id", StringType()),
+        StructField("title", StringType()),
+        StructField("publish_time", TimestampType()),
+        StructField("content", StringType()),
+    ]
+)
