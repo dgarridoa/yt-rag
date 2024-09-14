21a3eea1ca24db5dba4d9f5714cdf5238e40aed6
Diego Alberto Garrido Amaya <diego.garrido.6568@gmail.com>
2024-09-13 21:24:05 -0300

    feat: display rag error in streamlit

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
 
-import requests
 import streamlit as st
 
+from yt_rag.api.app import get_rag_response
 from yt_rag.settings import get_settings
 
 settings = get_settings()
 url = os.getenv("API_ENDPOINT", "http://localhost:5000/rag")
+
 
 st.title("💬 Chatbot")
 st.caption("🚀 RAG powered by LLama 3.1")
@@ -24,19 +25,18 @@ for msg in st.session_state.messages:
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = requests.post(
+    response = get_rag_response(
         url,
-        auth=(
-            settings.api_username.get_secret_value(),
-            settings.api_password.get_secret_value(),
-        ),
-        params={"input": st.session_state.messages[-1]["content"]},
-        timeout=120,
-    ).json()
-    msg = response["answer"]
-    st.session_state.messages.append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg)
-    video_url = (
-        f'https://www.youtube.com/watch?v={response["context"][0]["video_id"]}'
+        settings.api_username.get_secret_value(),
+        settings.api_password.get_secret_value(),
+        st.session_state.messages[-1]["content"],
     )
-    st.video(video_url)
+    if "error" in response:
+        st.error("An error occurred")
+        st.json(response["response"])
+    else:
+        msg = response["answer"]
+        st.session_state.messages.append({"role": "assistant", "content": msg})
+        st.chat_message("assistant").write(msg)
+        video_url = f'https://www.youtube.com/watch?v={response["context"][0]["video_id"]}'
+        st.video(video_url)
